5ba319ac0307ac311396f3207219bbbc7a7b60ef
Erik Ahumada <costerik@hotmail.com>
2016-04-21 23:49:56 -0500

    Create home fragment but without Firebase due to conflict with gradle

--- app/src/main/java/com/example/ingerikahumada/stuff/MainActivity.java
+++ app/src/main/java/com/example/ingerikahumada/stuff/MainActivity.java
@@ -3,16 +3,21 @@ package com.example.ingerikahumada.stuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.firebase.client.Firebase;
+//import com.firebase.client.Firebase;
 
 public class MainActivity extends AppCompatActivity {
 
-    Firebase m_fb;
+    //Firebase m_fb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Firebase.setAndroidContext(this);
+        //Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_main);
+
+        Home home= new Home();
+
+        //getFragmentManager().beginTransaction().add(R.id.fragment_container, home).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, home).commit();
     }
 }
