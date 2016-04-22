28f4e75b3a752756955079b8e2287f5eb1025a42
Erik Ahumada <costerik@hotmail.com>
2016-04-21 23:00:06 -0500

    Create the main activity and Firebase dependencies without fragments

---
+++ app/src/main/java/com/example/ingerikahumada/stuff/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.ingerikahumada.stuff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.firebase.client.Firebase;
+
+public class MainActivity extends AppCompatActivity {
+
+    Firebase m_fb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
+        setContentView(R.layout.activity_main);
+    }
+}
